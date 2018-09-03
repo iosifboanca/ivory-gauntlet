189c392b9091302d90e68e2710551b0152e4c9de
iosifboanca <iosifboanca@gmail.com>
2018-09-03 22:16:07 +0300

    minHeap added

--- #1day-data-structures/index.js
+++ #1day-data-structures/index.js
@@ -5,7 +5,7 @@ class Stack {
   }
 
   push(val) {
-    if (val!==undefined){
+    if (val !== undefined) {
       this.storage[this.count] = val
       this.count++
     }
@@ -36,4 +36,58 @@ class Stack {
   }
 }
 
-module.exports = { Stack }
+class MinHeap {
+  //Playground for vizualization - https://www.cs.usfca.edu/~galles/visualization/Heap.html
+  constructor() {
+    this.heap = [null]
+  }
+  insert(num) {
+    this.heap.push(num)
+    if (this.heap.length > 2) {
+      let index = this.heap.length - 1
+      while (this.heap[index] < this.heap[Math.floor(index / 2)]) {
+        if (index >= 1) {
+          [this.heap[Math.floor(index / 2)], this.heap[index]] = [this.heap[index], this.heap[Math.floor(index / 2)]]
+          if (Math.floor(index / 2) > 1) {
+            index = Math.floor(index / 2)
+          } else {
+            return
+          }
+        }
+      }
+    }
+  }
+
+  remove() {
+    let min = this.heap[1]
+    if (this.heap.length > 2) {
+      this.heap[1] = this.heap[this.heap.length - 1]
+      this.heap.length--
+      let i = 1
+      while (this.heap[i] >= this.heap[2 * i] || this.heap[i] >= this.heap[2 * i + 1]) {
+        if (this.heap[2 * i] < this.heap[2 * i + 1]) {
+          [this.heap[i], this.heap[2 * i]] = [this.heap[2 * i], this.heap[i]]
+          i *= 2
+        } else {
+          [this.heap[i], this.heap[2 * i + 1]] = [this.heap[2 * i + 1], this.heap[i]]
+          i = 2 * i + 1
+        }
+        if (this.heap[2 * i] == undefined || this.heap[2 * i + 1] == undefined)
+          break
+      }
+    } else if (this.heap.length == 2) {
+      this.heap.length=1
+    } else {
+      this.heap[0]
+    }
+    return min
+  }
+}
+
+class MaxHeap {
+  constructor() {
+    this.heap = [null]
+  }
+}
+
+module.exports = { Stack, MinHeap }
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
+SUMMARY MENTIONS 
 
+Due to the lack of previous experience with or comprehensive understanding of what *data strctures* are or how they interfere with programming, this project may contain more than one data structure presented, at the end, only one will be selected to be presented as representative, at the choice of the developer. 
+This process was created more for the process of learning other than having a final product to present. This is purely facultative and represents a non-official, non-related to initial instructions incursion and it has no expectations to be percieved on the top the selected item to be presented. Therefore, the reader is kindly asked to ignore the surplus and to get directly to the point of interest. Thank you!
+
+NOTES TO SELF:
+
+  When it comes to Min/MaxHeaps the *key secret is understanding the clearly dichotomization in left and right*
+and the *implicit multiplicity with 2*, *respectively 2+1*(to the floor of the result). Therefore to determine 
+the parent node by reverse dividing to 2. All this has to be in accordance with 
+the index(position) of the wanted item.
+
+  Ex. [null,1,3,6,7,8,9,12] will be represented as:
+        1
+       3 6
+     7 8 9 12
+left child of 3 will be on position 2*2(the position of 3), therefore it will be 7, add 1 and you'll have the right children;
